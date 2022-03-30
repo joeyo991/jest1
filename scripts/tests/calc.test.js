8e20116265b7630d49157b59d72dcfbb7868843a
joeyo991 <joeyosull@gmail.com>
2022-03-30 12:33:42 +0000

    Writing Our First Test

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,19 @@
+const { TestWatcher } = require("jest");
+const addition = require("../calc");
+
+describe("Calculator", () => {
+    describe("Addition function", () => {
+        test("Should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        })
+    });
+    describe("Subtraction function", () => {
+
+    });
+    describe("Multiplication function", () => {
+
+    });
+    describe("Division function", () => {
+
+    });
+})
